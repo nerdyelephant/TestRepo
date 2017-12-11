d5ef961abaa44e7be9dd7bea35c79de2afc4d9f2
Liao <liaoe@acbc32d4a9cb.ant.amazon.com>
2017-12-11 14:47:24 -0800

    add
    add

---
+++ TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Liao, Ej on 12/11/17.
+//  Copyright © 2017 Liao, Ej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestApp/TestApp/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  TestApp
+//
+//  Created by Liao, Ej on 12/11/17.
+//  Copyright © 2017 Liao, Ej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
