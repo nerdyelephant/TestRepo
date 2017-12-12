7ea458b5e3f058b6bc0bdf03f928b3a3db5a543a
Liao <liaoe@acbc32d4a9cb.ant.amazon.com>
2017-12-12 02:46:30 -0800

    FirstAIR App

---
+++ FirstAR/FirstAR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstAR
+//
+//  Created by Liao, Ej on 12/12/17.
+//  Copyright © 2017 NerdyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstAR/FirstAR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstAR
+//
+//  Created by Liao, Ej on 12/12/17.
+//  Copyright © 2017 NerdyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
